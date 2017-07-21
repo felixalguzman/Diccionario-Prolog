9ce6eb545f13c833d52aeaebd2ca9ea051a00992
Felix Guzman <Felix Guzman@FelixGuzman>
2017-07-21 11:32:18 -0400

    Creando conexcion

---
+++ PL/Diccionario.pl
@@ -0,0 +1,90 @@
+%Diccionario
+
+%palabra(palabra, Definicion, )
+palabra(Abaco,Cuadro de madera con diez cuerdas o 
+alambres paralelos y en cada uno de ellos 
+otras tantas bolas movibles, usado en las 
+escuelas para enseñar a los niños los rudi- 
+mentos de la aritmética,)
+
+Abaco. (Del lat. abáeus, y ¿ste del gr. d6aV) 
+
+m. Cuadro de madera con diez cuerdas o 
+alambres paralelos y en cada uno de ellos 
+otras tantas bolas movibles, usado en las 
+escuelas para enseñar a los niños los rudi- 
+mentos de la aritmética. || ' Arq. I'arte supe- 
+rior en forma de tablero, que corona el capi- 
+tel. II ' Min. Artesa que se usa en las minas 
+para lavar los metales, especialmente el oro. 
+Abad. (Del lal. abbiu, -tUit, y este del siriaco 
+K3K, abia, padre.) m. Superior de un mo- 
+nasterio. II ' Superior de una colegiata. || iF.n 
+Galicia, Navarra y otras provincias, cura, 
+I." acep. II * Cura o beneficiado elegido por 
+sus compañeros para que los presida en ca- 
+bildo durante cierto tiempo. || ' Titulo hono- 
+rífico de la persona lega que, por derecho 
+de sucesión, posee algima abadia con frutos 
+secularizados. || • Hermano mayor de una 
+cofradía. || ' En los antiguos cabildos de al- 
+gunas catedrales, titulo de una dignidad. || 
+' Capitán o caudillo de la guardia que lla- 
+maban del Conde Don Ciómcz, la cual se 
+componía de un abad, (juc era caballero, y 
+de cincuenta ballesteros, que eran hijosdal- 
+go. II » Abadejo, }.' acep. || lo \'. Or^la de 
+abad. |l Abad avariento, por un bodigo pier- 
+de ciento, ref. con que se da a entender que 
+la avaricia redunda por lo común en perjui- 
+cio del mismo avaro. || AtMid de Zarzuela, 
+comUteU la olla, pedia la cazuela, ref. que 
+rejirende a los qui- n ii^miIos con lo ne- 
+
+Abadia. (Del lat. atbaiU.) f. Dignidad de 
+abad o de abadesa. || * Iglesia o monasterio 
+ilel abad o de la abadesa. || ' Territorio, ju- 
+risdicción y bienes o rentas pertenecientes 
+al abad o a la abadesa. || ♦ En Galicia, Na- 
+varra y otras provincias, casa del cura. [I 
+' Especie de luctuosa que en algunos pun- 
+tos, especialmente en Galicia, se paga al pá- 
+rroco a la muerte de un feligrés. 
+
+Abadiado, m. Abadia, i.*, 2.* y 3.* aceps.(| 
+• .4r. Territorio de la abadía. 
+
+Abanicar, a. Hacer aire con el abanico. 
+
+t '. m. c. r. 
+
+Abanicazo. m. Golpr dado con el aba- 
+nico. 
+
+Abanico, (d. de ataat.) m. Instrumento 
+para hacer o hacerse aire. lil más &lt; umún hoy 
+tiene pie de varillas y país de (■■la, papel o 
+piel y se abre formando semicirciiio. || ' fig. 
+Cosa de figura de aliaalco, como la cola del 
+j&gt;avo real. || » fig. y fam. Sable, I." art. ;¡ 
+
+♦ Germ. Espada, i.* acep. || ' Mar. Especie 
+de cabria para suspender grandes i)esos, que 
+ordinariamente se forma a bordo con un 
+palo inclinado hacia afuera y sujeto por el 
+pie a otro vertical. || Ea abanico, m. adv. En 
+forma de abanico. || Parecer uno abanico 
+de tonta, fr. fig. y, fam. Moverse mucho y 
+sin concierto. 
+
+Abanillo, (d. d« ataña.) m. Adorno de 
+lienzo afollado de que se formaban ciertos 
+&lt; U'-llos alechugados. || * Abanico, i.* acep. 
+
+Abanino. (d. de aiam.) m. I'orción de 
+gasa u otra tela blanca con que ciertas da- 
+mas de la corte guarnecían en ondas el es- 
+cote del jubón. 
+
+Abaniqueo, m. Acción de abanicar o 
+abanicarse mucho o muy de prisa. 
---
+++ src/prueba/prolog/PruebaProlog.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba.prolog;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import org.jpl7.Query;
+import org.jpl7.Util;
+import org.jpl7.fli.atom_t;
+
+
+/**
+ *
+ * @author Felix Guzman
+ */
+public class PruebaProlog {
+
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+        
+         ArrayList<String> items = new ArrayList();
+         
+        try {
+            
+            
+            String t1 = "consult('PL/laberinto.pl')";
+
+            Query q = new Query(t1);
+            System.out.println(t1 + " " + (q.hasSolution() ? "correcto" : "fallo"));
+            
+          
+           
+           String s = "sol(A)";
+           Query q2 = new Query(s);
+           String lista[];
+           
+           
+            lista = Util.atomListToStringArray( q2.oneSolution().get("A"));
+            
+            for (int i = 0; i < lista.length; i++) {
+                items.add(lista[i].toString());
+            }
+                   
+           
+           
+            for (String item : items) {
+                
+                System.out.println("Solucion: " + item);
+            }
+        
+        } catch (Exception e) {
+        }
+       
+        
+    }
+    
+}
